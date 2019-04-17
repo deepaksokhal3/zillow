e5f3b8c3b7f1213c88aa335ffb2f943c90d7ce26
gulsharn <rvk_singh@outlook.com>
2019-04-17 23:57:27 +0530

    append array

---
+++ improveObject.php
@@ -0,0 +1,89 @@
+<?php
+error_reporting(-1);
+if (isset($_POST['submit'])) {
+
+	if ($_POST['address']) {
+		$params = '&address=' . str_replace(' ', '+', trim($_POST['address']));
+	}
+	if ($_POST['citystatezip']) {
+		$params .= '&citystatezip=' . urlencode(str_replace(' ', '+', $_POST['citystatezip']));
+	}
+
+	$zwsId = trim($_POST['zwsId']);
+}
+$url = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=' . $zwsId . $params;
+$jsonObject = '{
+    "identity": {
+    "addresses": [
+    {
+    "data": {
+    "zip": "94114",
+    "state": "CA",
+    "city": "San Francisco",
+    "street": "3819 Greenhaven Ln"
+    },
+    "primary": true
+    }
+    ],
+    "emails": [
+    {
+    "primary": true,
+    "type": "personal",
+    "data": "kelly.walters30@example.com"
+    }
+    ],
+    "names": [
+    "Kelly Walters"
+    ],
+    "phone_numbers": [
+    {
+    "primary": true,
+    "type": "home",
+    "data": "4673956022"
+    }
+    ]
+    }
+    }';
+ini_set('display_errors', 'On');
+function getXml($path) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $path);
+	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+	$retValue = curl_exec($ch);
+	curl_close($ch);
+	return $retValue;
+}
+$sXML = getXml($url);
+//
+$aux = !empty($sXML) ? explode('ï»¿', $sXML) : NULL;
+$temp = utf8_decode(trim($aux[0]));
+$xml = (array) simplexml_load_string($temp);
+$exitingBoject = json_decode($jsonObject);
+$exist = (array) $exitingBoject->identity;
+if (isset($xml['response'])) {
+	foreach ($xml['response']->results as $result) {
+		if (count($result) > 0) {
+			foreach ($result as $key => $unitdetails) {
+				$new = (array) $unitdetails;
+				foreach ($new as $key => $newValue) {
+					if ($key == 'address') {
+						array_push($exist['addresses'], $newValue);
+					} else {
+						$update = (array) $newValue;
+						if (isset($exist[$key])) {
+							array_push($exist[$key], $update);
+						} else {
+							$exist[$key][] = (array) $newValue;
+						}
+
+					}
+				}
+				$exitingBoject->identity = $exist;
+			}
+		}
+	}
+}
+echo json_encode($exitingBoject);
--- index.php
+++ index.php
@@ -1,81 +1,32 @@
-
-<?php
-error_reporting(-1);
-
-$jsonObject = '{
-  "identity": {
-    "addresses": [
-      {
-        "data": {
-          "zip": "94114",
-          "state": "CA",
-          "city": "San Francisco",
-          "street": "3819 Greenhaven Ln"
-        },
-        "primary": true
-      }
-    ],
-    "emails": [
-      {
-        "primary": true,
-        "type": "personal",
-        "data": "kelly.walters30@example.com"
-      }
-    ],
-    "names": [
-      "Kelly Walters"
-    ],
-    "phone_numbers": [
-      {
-        "primary": true,
-        "type": "home",
-        "data": "4673956022"
-      }
-    ]
-  }
-}';
-
-ini_set('display_errors', 'On');
-
-function download_page($path) {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $path);
-	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-	$retValue = curl_exec($ch);
-	curl_close($ch);
-	return $retValue;
-}
-function simpleXmlToArray($xmlObject) {
-	$array = [];
-	foreach ($xmlObject->children() as $node) {
-		$array[$node->getName()] = is_array($node) ? simplexml_to_array($node) : (string) $node;
-	}
-	return $array;
-}
-$sXML = download_page('http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1h0xp17wjd7_4zss3&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA');
-//
-$aux = !empty($sXML) ? explode('ï»¿', $sXML) : NULL;
-$temp = utf8_decode(trim($aux[0]));
-$xml = (array) simplexml_load_string($temp);
-$exitingBoject = json_decode($jsonObject);
-
-foreach ($xml['response']->results as $result) {
-	if (count($result) > 0) {
-		foreach ((array) $result as $key => $unitdetails) {
-			$exist = (array) $exitingBoject->identity;
-			$new = (array) $unitdetails;
-			foreach ($new as $key => $newValue) {
-				if (array_key_exists($exist, $key)) {
-					array_push($exist, $new);
-				}
-				echo '<pre>';
-				print_r($exist['addresses']);
-				echo '</pre>';
-			}
-		}
-	}
-
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Zillow</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <h2>Serach </h2>
+      <form action="improveObject.php" method="post">
+        <div class="form-group">
+          <label for="zwsId">zwsId</label>
+          <input type="text" class="form-control" placeholder="Enter zws id" name="zwsId" required>
+        </div>
+        <div class="form-group">
+          <label for="email">address</label>
+          <input type="text" class="form-control" placeholder="Enter address" name="address" required>
+        </div>
+        <div class="form-group">
+          <label for="pwd">citystatezip</label>
+          <input type="text" class="form-control" id="pwd" placeholder="Enter citystatezip" name="citystatezip">
+        </div>
+        <input  type="submit" name="submit" class="btn btn-primary" value="submit">
+      </form>
+    </div>
+  </body>
+</html>
